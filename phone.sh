838cfd2e267836214713d79bb83b51f0850cd8ce
qw46478 <46646584+qw46478@users.noreply.github.com>
2019-04-14 15:31:12 +0200

    Add files via upload

---
+++ phone.sh
@@ -0,0 +1,9 @@
+echo -e "\033[92m"
+
+for i in {2839103..3301047}; do
+    echo 094$i
+done
+echo "Done ..."
+echo -e " Ahsan Alsaeh @Libya@ "
+echo -e " Ahmed Albosifi :::_-_-_-_-_-_-_-_ "
+echo -e "Hacker's &Libya ! "
